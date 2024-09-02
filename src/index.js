e069b555fdf2607ffc15a5e77532c9891cd9860c
DiegowSantos93 <diegowillian41@gmail.com>
2024-09-02 20:42:42 -0300

    finish

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const cep_endereco = require("./middlewares/cep_endereco.js")
 const mercado_controller = require("./controllers/mercado.js")
 const mercado_router = require("./routes/mercado.js")
 const fornecedor_router = require("./routes/fornecedor.js")
+const produto_router = require("./routes/produto.js")
 const app = express();
 const port = 9900
 
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use("/mercado", mercado_router)
 
 app.use("/fornecedor", fornecedor_router)
+
+app.use("/produto", produto_router)
 
 app.listen(port, () => {
     console.log(`Server running in ${port} port`)
--- src/models/produto.js
+++ src/models/produto.js
@@ -1,5 +1,24 @@
+const mercado_controller = require("../controllers/mercado.js")
+const fornecedor_controller = require("../controllers/fornecedor.js")
+
 let nextId = 1;
 
 const model = (body, id = nextId++) => {
-    
+
+    console.log(body)
+    if (body.nome != undefined  && body.nome != "" &&
+        body.quantidade != undefined &&body.quantidade > 0 &&
+        mercado_controller.show(body.mercado_id) &&
+        fornecedor_controller.show(body.fornecedor_id)
+    ){
+        return {
+            id,
+            nome: body.nome,
+            quantidade: body.quantidade,
+            mercado_id: body.mercado_id,
+            fornecedor_id: body.fornecedor_id
+        }
+    }
 }
+
+module.exports = model
