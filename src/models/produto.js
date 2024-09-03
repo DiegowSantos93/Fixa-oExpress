0b55689dda4c427b25b903f2979126e53ae7da0b
Diego Willian <diegowillian41@gmail.com>
2024-09-02 21:54:47 -0300

    Update produto.js

--- src/models/produto.js
+++ src/models/produto.js
@@ -5,8 +5,8 @@ let nextId = 1;
 
 const model = (body, id = nextId++) => {
 
-    if (body.nome != undefined  && body.nome != "" &&
-        body.quantidade != undefined &&body.quantidade > 0 &&
+    if (body.nome != undefined && body.nome != "" &&
+        body.quantidade != undefined && body.quantidade > 0 &&
         mercado_controller.show(body.mercado_id) &&
         fornecedor_controller.show(body.fornecedor_id)
     ){
